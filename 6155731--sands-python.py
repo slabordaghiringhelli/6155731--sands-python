7ceee2db2dd4c30c7c417cecec92fc3888ebf958
Sergi <slabordaghiringhelli@gmail.com>
2025-09-22 17:28:00 +0200

    sine wave function added

--- 6155731--sands-python.py
+++ 6155731--sands-python.py
@@ -1,1 +1,16 @@
 print("Hello!")
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_sine_wave(frequency, duration):
+    t = np.linspace(0, duration, int(44100 * duration))
+    return np.sin(2 * np.pi * frequency * t)
+
+sine_wave = create_sine_wave(440, 2)
+
+plt.plot(sine_wave)
+
+
+
