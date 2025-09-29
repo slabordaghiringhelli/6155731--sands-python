956acf70b488033ac733a8baae37831181ac4080
Sergi <slabordaghiringhelli@gmail.com>
2025-09-29 16:57:20 +0200

    trying

--- 6155731--sands-python.py
+++ 6155731--sands-python.py
@@ -4,18 +4,24 @@ print("Hello!")
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
 def create_sine_wave(frequency, duration):
     t = np.linspace(0, duration, int(44100 * duration))
     y = np.sin(2 * np.pi * frequency * t)
     return t, y
+
+
 t, y = create_sine_wave(440, 2)
 
 
-plt.plot(t, y)
 
-plt.xlabel("Time (seconds)")
+plt.plot(t,y)
+plt.xlabel("Time(seconds)")
 plt.ylabel("Amplitude")
-plt.title("sine wave")
+plt.title("Sine wave")
 plt.show()
+
+
 
 
