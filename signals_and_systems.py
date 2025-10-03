04558e9edc707978007065d47fb0611534865fef
Sergi <slabordaghiringhelli@gmail.com>
2025-10-03 14:12:18 +0200

    changes on the code of the sine function

--- signals_and_systems.py
+++ signals_and_systems.py
@@ -8,15 +8,15 @@ import matplotlib.pyplot as plt
 
 def create_sine_wave(frequency, duration):
     t = np.linspace(0, duration, int(44100 * duration))
-    y = np.sin(2 * np.pi * frequency * t)
-    return t, y
+    return np.sin(2 * np.pi * frequency * t)
+    
 
 
-t, y = create_sine_wave(440, 2)
+sine_wave = create_sine_wave(440, 2)
 
 
 
-plt.plot(t,y)
+plt.plot(sine_wave)
 plt.xlabel("Time(seconds)")
 plt.ylabel("Amplitude")
 plt.title("Sine wave")
